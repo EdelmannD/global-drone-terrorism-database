ac6185488d730b17b93d5c019f736f972b9e18dd
EdelmannD <edelmann.dora@phd.uni-obuda.hu>
2026-04-30 20:31:02 +0200

    Update app.py

--- app.py
+++ app.py
@@ -270,8 +270,8 @@ if not df_raw.empty:
     st.markdown("""
         <div class="footer-note">
             Data sources: <br>
-            START - National Consortium for the Study of Terrorism and Responses to Terrorism, Global Terrorism Database, 1970 - 2022, 2025 database, 2025. <br>
-            C. Raleigh, A. Linke, H. Hegre, J. Karlsen, Introducing ACLED: An armed conflict location and event dataset, J. Peace Res. 47, 2010.
+            GTD: START - National Consortium for the Study of Terrorism and Responses to Terrorism, Global Terrorism Database, 1970 - 2022, 2025 database, 2025. <br>
+            ACLED: C. Raleigh, A. Linke, H. Hegre, J. Karlsen, Introducing ACLED: An armed conflict location and event dataset, J. Peace Res. 47, 2010.
         </div>
     """, unsafe_allow_html=True)
 else:
